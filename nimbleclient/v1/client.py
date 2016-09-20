ef6af839ad7710c63bb1b876c45a8dc65589e101
Rui Chen <chenrui.momo@gmail.com>
2016-09-20 17:19:56 +0800

    Add HttpClient and OSC plugin
    Change-Id: I272dbaf877dbdfbfa7ebb67dbf2d9c3dd3d7246d

---
+++ nimbleclient/v1/client.py
@@ -0,0 +1,24 @@
+#   Copyright 2016 Huawei, Inc. All rights reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from nimbleclient.common import http
+
+
+class Client(object):
+    """Client for the Nimble v1 API."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new client for the Nimble v1 API."""
+        self.http_client = http._construct_http_client(*args, **kwargs)
