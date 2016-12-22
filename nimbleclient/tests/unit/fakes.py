2777e781f131c733c4f12eddd7a14d410ac1d605
liusheng <liusheng@huawei.com>
2016-12-22 20:18:44 +0800

    Refactor: change *instance* to *server* (part1)
    Change-Id: I088e7e1943f989b9b7e9bb85cf309e93306ff065

--- nimbleclient/tests/unit/fakes.py
+++ nimbleclient/tests/unit/fakes.py
@@ -22,8 +22,8 @@ from requests import Response
 
 from nimbleclient.common import base
 from nimbleclient.v1 import availability_zone
-from nimbleclient.v1 import instance
 from nimbleclient.v1 import instance_type
+from nimbleclient.v1 import server
 
 
 # fake request id
@@ -62,7 +62,7 @@ class FakeBaremetalComputeV1Client(object):
 
         self.instance_type = instance_type.InstanceTypeManager(
             self.fake_http_client)
-        self.instance = instance.InstanceManager(self.fake_http_client)
+        self.instance = server.ServerManager(self.fake_http_client)
         self.availability_zone = availability_zone.AvailabilityZoneManager(
             self.fake_http_client)
 
