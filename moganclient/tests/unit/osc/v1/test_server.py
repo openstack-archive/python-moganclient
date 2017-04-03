518a1777a9dfd20494a36910b17cd4890678919a
DeepaJon <deepak.os31@yahoo.com>
2017-04-03 18:45:22 +0530

    Replace uuid.uuid4() with uuidutils.generate_uuid()
    As of now, UUID is being generated using either uuid.uuid4()
    or uuidutils.generate_uuid(). In order to maintain consistency,
    we propose to use uuidutils.generate_uuid() from oslo_utils.
    
    Change-Id: I60ed3771d98a9c9893750d02d601c0dbaaec393b
    Closes-bug: #1082248

--- moganclient/tests/unit/osc/v1/test_server.py
+++ moganclient/tests/unit/osc/v1/test_server.py
@@ -15,10 +15,10 @@
 
 import copy
 import mock
-import uuid
 
 from osc_lib import exceptions
 from osc_lib import utils
+from oslo_utils import uuidutils
 
 from moganclient.osc.v1 import server
 from moganclient.tests.unit import base as test_base
@@ -142,17 +142,19 @@ class TestServerCreate(TestServer):
 
     def test_server_create(self, mock_create, mock_find):
         name = 'server1'
-        flavor_id = 'flavor-id-' + uuid.uuid4().hex
-        image_id = 'image-id-' + uuid.uuid4().hex
-        networks = [{'net-id': 'net-id-' + uuid.uuid4().hex}]
+        flavor_id = 'flavor-id-' + uuidutils.generate_uuid(dashed=False)
+        image_id = 'image-id-' + uuidutils.generate_uuid(dashed=False)
+        networks = [{'net-id': 'net-id-' + uuidutils.generate_uuid(
+              dashed=False)}]
         self._test_create_fake_server(mock_create, mock_find,
                                       name, flavor_id, image_id, networks)
 
     def test_server_create_with_description(self, mock_create, mock_find):
         name = 'server1'
-        flavor_id = 'flavor-id-' + uuid.uuid4().hex
-        image_id = 'image-id-' + uuid.uuid4().hex
-        networks = [{'net-id': 'net-id-' + uuid.uuid4().hex}]
+        flavor_id = 'flavor-id-' + uuidutils.generate_uuid(dashed=False)
+        image_id = 'image-id-' + uuidutils.generate_uuid(dashed=False)
+        networks = [{'net-id': 'net-id-' + uuidutils.generate_uuid(
+              dashed=False)}]
         description = 'fake_description'
         self._test_create_fake_server(mock_create, mock_find,
                                       name, flavor_id, image_id,
@@ -160,9 +162,10 @@ class TestServerCreate(TestServer):
 
     def test_server_create_with_az(self, mock_create, mock_find):
         name = 'server1'
-        flavor_id = 'flavor-id-' + uuid.uuid4().hex
-        image_id = 'image-id-' + uuid.uuid4().hex
-        networks = [{'net-id': 'net-id-' + uuid.uuid4().hex}]
+        flavor_id = 'flavor-id-' + uuidutils.generate_uuid(dashed=False)
+        image_id = 'image-id-' + uuidutils.generate_uuid(dashed=False)
+        networks = [{'net-id': 'net-id-' + uuidutils.generate_uuid(
+              dashed=False)}]
         fake_az = 'fake_availability_zone'
         self._test_create_fake_server(mock_create, mock_find,
                                       name, flavor_id, image_id,
@@ -170,19 +173,21 @@ class TestServerCreate(TestServer):
 
     def test_server_create_with_port_type(self, mock_create, mock_find):
         name = 'server1'
-        flavor_id = 'flavor-id-' + uuid.uuid4().hex
-        image_id = 'image-id-' + uuid.uuid4().hex
-        networks = [{'net-id': 'net-id-' + uuid.uuid4().hex,
-                     'port-type': 'normal'}]
+        flavor_id = 'flavor-id-' + uuidutils.generate_uuid(dashed=False)
+        image_id = 'image-id-' + uuidutils.generate_uuid(dashed=False)
+        networks = [{'net-id': 'net-id-' + uuidutils.generate_uuid(
+              dashed=False),
+              'port-type': 'normal'}]
         self._test_create_fake_server(mock_create, mock_find,
                                       name, flavor_id, image_id,
                                       networks)
 
     def test_server_create_with_extra(self, mock_create, mock_find):
         name = 'server1'
-        flavor_id = 'flavor-id-' + uuid.uuid4().hex
-        image_id = 'image-id-' + uuid.uuid4().hex
-        networks = [{'net-id': 'net-id-' + uuid.uuid4().hex}]
+        flavor_id = 'flavor-id-' + uuidutils.generate_uuid(dashed=False)
+        image_id = 'image-id-' + uuidutils.generate_uuid(dashed=False)
+        networks = [{'net-id': 'net-id-' + uuidutils.generate_uuid(
+              dashed=False)}]
         extra_info = 'key1=test'
         self._test_create_fake_server(mock_create, mock_find,
                                       name, flavor_id, image_id,
