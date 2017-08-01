bc04d6a5c37ba0f651f9329f47aa37bf11c3af77
Tao Li <litao3721@126.com>
2017-08-01 16:40:19 +0800

    Add the CLI for managing server
    Add 'openstack baremetal server manage' command for managing an
    existing baremetal node.
    
    Change-Id: I3d950511fbd37f129473a4d509e69280ac7572ff

--- moganclient/osc/v1/manageable_server.py
+++ moganclient/osc/v1/manageable_server.py
@@ -18,6 +18,7 @@
 
 import logging
 
+from osc_lib.cli import parseractions
 from osc_lib.command import command
 from osc_lib import utils
 
@@ -90,10 +91,78 @@ class ListManageableServer(command.Lister):
                 "resource_class"
             )
 
-        data = bc_client.manageable_servers.list()
+        data = bc_client.manageable_server.list()
         formatters = {'ports': self._ports_formatter,
                       'portgroups': self._ports_formatter
                       }
         return (column_headers,
                 (utils.get_item_properties(
                     s, columns, formatters=formatters) for s in data))
+
+
+class ManageServer(command.ShowOne):
+    """Manage an existing bare metal node"""
+
+    def get_parser(self, prog_name):
+        parser = super(ManageServer, self).get_parser(prog_name)
+        parser.add_argument(
+            "name",
+            metavar="<name>",
+            help=_("New bare metal server name")
+        )
+        parser.add_argument(
+            "--node_uuid",
+            metavar="<node>",
+            help=_("The manageable bare metal node uuid")
+        )
+        parser.add_argument(
+            "--description",
+            metavar="<description>",
+            help=_("Bare metal server description"),
+        )
+        parser.add_argument(
+            '--property',
+            metavar='<key=value>',
+            action=parseractions.KeyValueAction,
+            help=_('Set a property on this server '
+                   '(repeat option to set multiple values)'),
+        )
+
+        return parser
+
+    def _format_image_field(self, data):
+        image_client = self.app.client_manager.image
+        image_uuid = data._info.pop('image_uuid')
+        if image_uuid:
+            image = image_client.images.get(image_uuid)
+            return '%s (%s)' % (image.name, image_uuid)
+
+    def _format_flavor_field(self, data):
+        bc_client = self.app.client_manager.baremetal_compute
+        flavor_uuid = data._info.pop('flavor_uuid')
+        if flavor_uuid:
+            flavor = bc_client.flavor.get(flavor_uuid)
+            return '%s (%s)' % (flavor.name, flavor_uuid)
+
+    def take_action(self, parsed_args):
+        bc_client = self.app.client_manager.baremetal_compute
+
+        boot_kwargs = dict(
+            name=parsed_args.name,
+            node_uuid=parsed_args.node_uuid,
+            description=parsed_args.description,
+            metadata=parsed_args.property
+        )
+
+        data = bc_client.manageable_server.manage(**boot_kwargs)
+        data._info.update(
+            {
+                'properties': utils.format_dict(data._info.pop('metadata')),
+                'flavor': self._format_flavor_field(data),
+                'image': self._format_image_field(data)
+            },
+        )
+
+        info = {}
+        info.update(data._info)
+        return zip(*sorted(info.items()))
