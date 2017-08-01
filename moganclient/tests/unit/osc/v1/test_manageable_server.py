fc7fa8cc989e245436ed9d8f61bea430219b5995
Tao Li <litao3721@126.com>
2017-08-01 16:40:19 +0800

    Add the CLI for managing server
    Add 'openstack baremetal server manage' command for managing an
    existing baremetal node.
    
    Change-Id: I3d950511fbd37f129473a4d509e69280ac7572ff
    Implements: bp manage-existing-bms

--- moganclient/tests/unit/osc/v1/test_manageable_server.py
+++ moganclient/tests/unit/osc/v1/test_manageable_server.py
@@ -17,11 +17,42 @@ import copy
 import json
 
 import mock
+from osc_lib import utils
 
 from moganclient.osc.v1 import manageable_server
 from moganclient.tests.unit import base as test_base
 from moganclient.tests.unit import fakes
 from moganclient.v1 import manageable_server as ms_mgr
+
+
+class TestManageableServer(test_base.TestBaremetalComputeV1):
+    fake_server = fakes.FakeServer.create_one_server()
+
+    columns = (
+        'addresses',
+        'availability_zone',
+        'created_at',
+        'description',
+        'flavor',
+        'image',
+        'links',
+        'name',
+        'node_uuid',
+        'properties',
+        'updated_at',
+        'uuid')
+
+    data = (
+        fake_server.addresses,
+        fake_server.availability_zone,
+        fake_server.created_at,
+        fake_server.description,
+        fake_server.flavor_uuid,
+        fake_server.image_uuid,
+        fake_server.links,
+        fake_server.name,
+        fake_server.updated_at,
+        fake_server.uuid)
 
 
 @mock.patch.object(ms_mgr.ManageableServerManager, '_list')
@@ -88,3 +119,64 @@ class TestManageableServerList(test_base.TestBaremetalComputeV1):
                            "755cac03-a460-4de0-8d8e-d1ac842768bf",
                            "baremetal", p_str, pg_str
                            ),), tuple(data))
+
+
+class TestServerManage(TestManageableServer):
+    def setUp(self):
+        super(TestServerManage, self).setUp()
+        self.cmd = manageable_server.ManageServer(self.app, None)
+        mocked_img = mock.Mock()
+        mocked_img.name = 'test-image'
+        image_mock = mock.MagicMock()
+        image_mock.images.get.return_value = mocked_img
+        self.app.client_manager.image = image_mock
+
+    def _test_manage_fake_server(self, mock_create, name, node,
+                                 description=None, properties=None):
+        arglist = [node, name]
+        verifylist = [
+            ('name', name),
+            ('node_uuid', node)]
+
+        called_data = {'name': name,
+                       'node_uuid': node}
+
+        if description:
+            arglist.extend(['--description', description])
+            verifylist.append(('description', description))
+            called_data['description'] = description
+        if properties:
+            arglist.extend(['--property', properties])
+            verifylist.append(('property', {'key1': 'test'}))
+            called_data['metadata'] = {'key1': 'test'}
+
+        fk_server = fakes.FakeServer.create_one_server(called_data)
+        fk_server._info['flavor_uuid'] = None
+
+        mock_create.return_value = fk_server
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        columns, data = self.cmd.take_action(parsed_args)
+        mock_create.assert_called_once_with(
+            '/manageable_servers',
+            data=called_data)
+        self.assertEqual(self.columns, columns)
+        expected_data = (
+            fk_server.addresses,
+            fk_server.availability_zone,
+            fk_server.created_at,
+            fk_server.description,
+            None,
+            'test-image (%s)' % fk_server.image_uuid,
+            fk_server.links,
+            fk_server.name,
+            fk_server.node_uuid,
+            utils.format_dict(fk_server.metadata),
+            fk_server.updated_at,
+            fk_server.uuid)
+        self.assertEqual(expected_data, data)
+
+    @mock.patch.object(ms_mgr.ManageableServerManager, '_create')
+    def test_server_manage(self, mock_create):
+        name = 'server1'
+        node = 'aacdbd78-d670-409e-95aa-ecfcfb94fee2'
+        self._test_manage_fake_server(mock_create, name, node)
